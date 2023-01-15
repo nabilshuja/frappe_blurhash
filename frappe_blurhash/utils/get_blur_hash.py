9972d8ae193c19077c8713f0d2a16897e082ae5a
Elton Lobo <elton1231@gmail.com>
2023-01-15 17:15:25 +0400

    refactor: validations

--- frappe_blurhash/utils/get_blur_hash.py
+++ frappe_blurhash/utils/get_blur_hash.py
@@ -7,18 +7,18 @@ def get_blur_hash(dt, dn, df):
     dn : Doc-name
     df : Doc-field
     """
+    if "frappe_blurhash" not in frappe.get_installed_apps():
+        return frappe.throw(frappe._("Frappe blurhash not installed in current site."))
+    fields = ["blurhash"]
     file_url = frappe.get_value(dt, dn, df)
     if frappe.is_table(dt):
         dt, dn = frappe.get_value(dt, dn,
                                   ("parenttype", "parent"))
-    attachments = get_attachments_by_docfield(dt, dn, df, file_url)
+    attachments = get_attachments_by_docfield(dt, dn, df, file_url, fields)
     return attachments[0].get("blurhash") if len(attachments) else None
 
 
-def get_attachments_by_docfield(dt, dn, df, file_url):
-    if "frappe_blurhash" not in frappe.get_installed_apps():
-        return []
-    fields = ["blurhash"]
+def get_attachments_by_docfield(dt, dn, df, file_url, fields):
     filters = {"attached_to_name": dn, "file_url": file_url,
                "attached_to_doctype": dt, "attached_to_field": df}
     files = frappe.get_all("File", fields=fields, filters=filters)
